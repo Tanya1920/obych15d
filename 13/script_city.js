524375c2ebe6b85fab25237712ba34b86895a47e
TatianaAlekhina <aleh.tan@yandex.ru>
2021-07-08 12:57:23 +0200

    SaiteD

---
+++ 13/script_city.js
@@ -0,0 +1,16 @@
+
+$(function f1(){
+
+ // b = $('#yourcity').click(function(){prompt('Ваш город  - Москва? (Да / Нет)')});
+ // jQuery.alerts.okButton = "ДА";
+let b = $('#yourcity').click(function(){confirm('Ваш город  - Москва? (Да / Нет)')});
+  console.log(b);
+  alert ($('#yourcity').click(function(){confirm('Ваш город  - Москва? (Да / Нет)')}));
+    // if (b = true) {
+	  //alert('москва');
+  //} else  alert ("НеМосква");
+	  
+  // alert(b);
+   
+	
+});
